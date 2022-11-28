32225902e5138362ca147d32247a250983ceebbc
Prominendt <hcebrink@gmail.com>
2022-11-28 20:20:25 +0100

    Wip

--- scripts/ssl/post_simpleenroll.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-    curl --insecure -s http://localhost:8000/.well-known/est/simpleenroll --user estuser:estpwd --output cert.p7 --header "Content-Type: application/pkcs10" --header "Content-Transfer-Encoding: base64" --data-binary @/home/prominendt/repos/ZEUS-services-CA/dev/certs/testcsr.pem
